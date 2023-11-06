cea25090a4d10e61e3e4bc9cf6cc32ecf3fde0ef
hokuto.hirano <hirano.hkt@gmail.com>
2023-11-06 22:24:41 +0900

    update

--- react-deckgl/README.md
+++ react-deckgl/README.md
@@ -1,4 +1,7 @@
 # Progress
 
 - [X] [The DeckGL React Component](https://deck.gl/docs/get-started/using-with-react#the-deckgl-react-component)
-- [ ] [Adding a Base Map](https://deck.gl/docs/get-started/using-with-react#adding-a-base-map)
+- [x] [Adding a Base Map](https://deck.gl/docs/get-started/using-with-react#adding-a-base-map)
+
+- [x] [【deck.gl】react-map-glを使って背景地図を表示する](https://qiita.com/asahina820/items/5e3212628184da8e8ee6)
+    - [x] [実装](https://qiita.com/asahina820/items/5e3212628184da8e8ee6#%E5%AE%9F%E8%A3%85)
--- react-deckgl/src/App.js
+++ react-deckgl/src/App.js
@@ -1,41 +1,42 @@
 /// app.js
 import React from 'react';
-import DeckGL from '@deck.gl/react';
 import { LineLayer } from '@deck.gl/layers';
-import { Map } from 'react-map-gl';
-
-// Set your mapbox access token here
-const MAPBOX_ACCESS_TOKEN = '';
-
-// Viewport settings
-const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0
-};
-
-// Data to be used by the LineLayer
-const data = [
-  { sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }
-];
+import { Map, useControl } from 'react-map-gl';
+import { MapboxOverlay } from '@deck.gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // DeckGL react component
 function App() {
+  const DeckGLOverlay = (props) => {
+    const overlay = useControl(() => new MapboxOverlay(props));
+    overlay.setProps(props);
+    return null
+  }
+
+  const MAPBOX_ACCESS_TOKEN = '';
+  const INITIAL_VIEW_STATE = {
+    latitude: 38.875584,
+    longitude: 139.7454316,
+    bearing: 0,
+    pitch: 0,
+    zoom: 5,
+  };
+  const data = [{ sourcePosition: [139.7454316, 38.875584], targetPosition: [145.7454316, 40.875584] }];
   const layers = [
     new LineLayer({ id: 'line-layer', data })
   ];
 
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-    // layers={layers}
-    >
-      <LineLayer id="line-layer" data={data}></LineLayer>
-      {/* <Map mapboxAccessToken={MAPBOX_ACCESS_TOKEN} /> */}
-    </DeckGL>
+    <div className="app">
+      <Map
+        initialViewState={INITIAL_VIEW_STATE}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        style={{ width: '100vw', height: '100vh' }}
+        mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+      >
+        <DeckGLOverlay layers={layers}></DeckGLOverlay>
+      </Map>
+    </div>
   );
 }
 
