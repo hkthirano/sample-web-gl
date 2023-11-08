1f2e7d290f05005792236254ec94eaade59a85cd
hokuto.hirano <hirano.hkt@gmail.com>
2023-11-08 22:58:28 +0900

    update

--- react-deckgl/src/App.jsx
+++ react-deckgl/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import { Content } from './components/content';
+// import { Content } from './components/content';
 import { Home } from './components/home';
 import { NotFound } from './components/not_found';
+import Basic from './components/basic'
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
         <h1>Hello world</h1>
         <menu>
           <li><Link to='/'>Home</Link></li>
-          <li><Link to='/content'>Content</Link></li>
+          <li><Link to='/basic'>Basic</Link></li>
+          {/* <li><Link to='/content'>Content</Link></li> */}
         </menu>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/content' element={<Content />} />
+          <Route path='/basic' element={<Basic />} />
+          {/* <Route path='/content' element={<Content />} /> */}
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
